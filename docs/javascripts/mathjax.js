769b8d3bc96e9ef7a42b56651fc4c93dc8c6dd7a
Mathias Garnier <mathias.garnier69000@gmail.com>
2023-01-29 18:43:03 +0100

    Suppression
    Suppression d'une mauvaise idée copiée collée à la va vite

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,6 +1,3 @@
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-
-
 window.MathJax = {
     tex: {
       inlineMath: [["\\(", "\\)"]],
